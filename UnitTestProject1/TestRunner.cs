287e09d56f40efeec19b1eebfd3b28e787a4ab6f
Franck Chauvel <franck.chauvel@gmail.com>
2015-10-07 10:46:28 +0200

    First commit, just a dummy test

---
+++ UnitTestProject1/TestRunner.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using heat;
+
+namespace UnitTestProject1
+{
+
+    class TestListener: Listener
+    {
+
+        public string[] CallSequence()
+        {
+            return new string[] { "burpees", "rest", "push-ups" };
+        }
+    }
+
+    [TestClass]
+    public class TestCoach
+    {
+        [TestMethod]
+        public void TestCoachRunCircuit()
+        {
+            Circuit circuit = new Circuit(new String[] { "burpees", "push-ups" });
+
+            TestListener listener = new TestListener();
+            Coach coach = new Coach(); 
+            coach.Run(circuit, listener);
+
+            CollectionAssert.AreEqual(listener.CallSequence(), new String[] { "burpees", "rest", "push-ups" });
+        }
+    }
+}
---
+++ heat/Engine.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace heat
+{
+    public class Circuit {
+
+        private string[] moves;
+
+        public Circuit(string[] moves)
+        {
+            this.moves = moves;
+        }
+    }
+
+    public class Coach
+    {
+        public Coach() { }
+
+        public void Run(Circuit circuit, Listener listener)
+        {
+
+        }
+    }
+
+    public class Listener
+    {
+
+    }
+}
